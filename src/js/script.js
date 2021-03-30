8f648876168c8e2ea25b2593ae441c102776592f
Jarek Gramacki <gramacki.programowanie@gmail.com>
2021-03-30 13:32:15 +0200

    Add event for accordion

--- src/js/script.js
+++ src/js/script.js
@@ -88,7 +88,7 @@
       /* START: add event listener to clickable trigger on event click */
       clickableTrigger.addEventListener('click', function(event) {
         /* prevent default action for event */
-        
+        event.preventDefault();
         /* find active product (product that has active class) */
   
         /* if there is active product and it's not thisProduct.element, remove class active from it */
