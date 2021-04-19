5b2ddb6755bfb34bc486b86cfffea773ddc20771
Jarek Gramacki <gramacki.programowanie@gmail.com>
2021-04-19 16:33:53 +0200

    Add initAmountWidget to CartProduct

--- src/js/script.js
+++ src/js/script.js
@@ -417,6 +417,7 @@
       thisCartProduct.params = menuProduct.params;
 
       thisCartProduct.getElements(element);
+      thisCartProduct.initAmountWidget();
       console.log('new CartProduct:', thisCartProduct);
     }
 
@@ -431,7 +432,19 @@
       thisCartProduct.dom.edit = element.querySelector(select.cartProduct.edit);
       thisCartProduct.dom.remove = element.querySelector(select.cartProduct.remove);
     }
-  }
+
+    initAmountWidget(){
+      const thisCartProduct = this;
+
+      thisCartProduct.amountWidget = new AmountWidget(thisCartProduct.dom.amountWidget);
+      thisCartProduct.dom.amountWidget.addEventListener('updated', function(){
+        thisCartProduct.amount = thisCartProduct.amountWidget.value;
+        thisCartProduct.price = thisCartProduct.priceSingle * thisCartProduct.amount; 
+        thisCartProduct.dom.price.innerHTML = thisCartProduct.price;
+
+      });
+    };
+  };
 
   const app = {
     initData: function(){
