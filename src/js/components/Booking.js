82f82482de248b945b723586a5c5885f33f17b94
Jarek Gramacki <gramacki.programowanie@gmail.com>
2021-07-04 14:24:03 +0200

    Add render to class Booking

--- src/js/components/Booking.js
+++ src/js/components/Booking.js
@@ -1,14 +1,29 @@
+/*global utils, templates */
 class Booking {
   constructor(element) {
     const thisBooking = this;
 
     thisBooking.getElements(element);
+    thisBooking.render(element);
+    thisBooking.initWidgets();
   }
 
   getElements(element) {
-    thisBooking = this;
+    const thisBooking = this;
 
     thisBooking.element = element; 
+  }
+
+  render(element) {
+    const thisBooking = this; 
+
+    const generatedHTML = templates.bookingWidget(thisBooking);
+
+    thisBooking.dom = {};
+    thisBooking.dom.wrapper = element; 
+
+    thisBooking.dom.wrapper = utils.createDOMFromHTML(generatedHTML); 
+
   }
 }
 
