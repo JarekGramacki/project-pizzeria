38b3abb990aa42e61135424f54ab074c7d191688
Jarek Gramacki <gramacki.programowanie@gmail.com>
2021-07-21 21:46:58 +0200

    next save

--- src/js/components/Booking.js
+++ src/js/components/Booking.js
@@ -1,5 +1,7 @@
 import { select, templates, } from '../settings.js';
 import AmountWidget from './AmountWidget.js';
+import DatePicker from './DatePicker.js';
+import HourPicker from './HourPicker.js';
 
 
 
@@ -53,12 +55,12 @@ class Booking {
     thisBooking.dom.hoursAmount.addEventListener('updated',function(){});
 
     
-    thisBooking.datePicker = new AmountWidget(
+    thisBooking.datePicker = new DatePicker(
       thisBooking.dom.datePicker
     );
     thisBooking.dom.datePicker.addEventListener('updated',function(){});
 
-    thisBooking.hourPicker = new AmountWidget(
+    thisBooking.hourPicker = new HourPicker(
       thisBooking.dom.hourPicker
     );
     thisBooking.dom.hourPicker.addEventListener('updated',function(){});
